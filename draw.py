545f9ac9358901ce54b556d6534a24a9b4211499
Mona Lisa <iamdheeraj2614@gmail.com>
2023-03-17 20:25:53 +0530

    2ndtime

--- draw.py
+++ draw.py
@@ -1,7 +1,6 @@
 import cv2 as cv
 import numpy as np
 
-
 blank=np.zeros((500,500,3),dtype='uint8')  # to create a blank image
 #cv.imshow('blank',blank)
 
@@ -14,7 +13,15 @@ blank=np.zeros((500,500,3),dtype='uint8')  # to create a blank image
 # cv.imshow('Green',blank)
 # cv.waitKey(0)
 
-#to draw a rectangle
-cv.rectangle(blank,(0,0),(250,250),(0,255,0),thickness=2)
-cv.imshow('rect',blank)
+# #to draw a rectangle
+# cv.rectangle(blank,(0,0),(250,250),(0,255,0),thickness=2)
+# cv.imshow('rect',blank)
+# cv.waitKey(0)
+
+
+
+#write text
+cv.putText(blank,'hello world!',(0,400),cv.FONT_HERSHEY_PLAIN,1.0,(0,250,0))
+cv.imshow('Text',blank)
 cv.waitKey(0)
+
